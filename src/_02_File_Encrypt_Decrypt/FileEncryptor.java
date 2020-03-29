acd724e8ff494c62f3d349a62662a90f61b07d6a
kevin <kevin@192.168.1.11>
2020-03-29 11:26:37 -0700

    uilsifdughlfoubdfs

--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -6,13 +6,14 @@ import java.io.IOException;
 import javax.swing.JOptionPane;
 
 public class FileEncryptor {
+	String alphabet = "abcdefghijklmnopqrstuvwxyz";
 	/*
-	 * Encryption is the process of encoding a message or information
-	 * in such a way that only authorized parties can access it and
-	 * those who are not authorized cannot.
+	 * Encryption is the process of encoding a message or information in such a way
+	 * that only authorized parties can access it and those who are not authorized
+	 * cannot.
 	 *
-	 * A simple shift cipher works by shifting the letters of a message
-	 * down based on a key. If our key is 4 then:
+	 * A simple shift cipher works by shifting the letters of a message down based
+	 * on a key. If our key is 4 then:
 	 * 
 	 * a b c d e f g h i j k l m n o p q r s t u v w x y z
 	 * 
@@ -22,20 +23,25 @@ public class FileEncryptor {
 	 * 
 	 * "Hello World" changes to "Lipps Asvph"
 	 *
-	 * Create a program that takes a message and a key from the user.
-	 * Use the key to shift each letter in the users input and save the final result to a file.
+	 * Create a program that takes a message and a key from the user. Use the key to
+	 * shift each letter in the users input and save the final result to a file.
 	 */
-	
-public static void main(String[] args) {
-	try {
-		FileWriter fw = new FileWriter("src/_02_File_Encryot_Decrypt/encryption.txt");
-		String m= JOptionPane.showInputDialog("Enter a message");
-		String k= JOptionPane.showInputDialog("Enter a key");
-		fw.write(m);
-			
-		fw.close();
-	} catch (IOException e) {
-		e.printStackTrace();
+
+	public static void main(String[] args) {
+		try {
+			FileWriter fw = new FileWriter("src/_02_File_Encryot_Decrypt/encryption.txt");
+			String m = JOptionPane.showInputDialog("Enter a message");
+			char[] arr = m.toCharArray();
+			String k = JOptionPane.showInputDialog("Enter a key");
+			String finai = " ";
+			for (int i = 0; i < arr.length; i++) {
+				
+			} 
+			fw.write(m);
+
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-}
 }
